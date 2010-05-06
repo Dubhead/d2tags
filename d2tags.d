75a8799369558de83566799362e2595385f03f8d
MIURA Masahiro <miura@accense.com>
2010-05-06 19:21:40 +0900

    first commit

---
+++ d2tags.d
@@ -0,0 +1,154 @@
+// Written in the D programming language.
+
+/**
+d2tags - converts DMD2's JSON output to Exuberant Ctags format.
+
+Copyright: Copyright MIURA Masahiro 2010 - 2010.
+License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+Authors:   $(WEB playrecord.org, MIURA Masahiro)
+References: $(LINK http://ctags.sourceforge.net/)
+
+         Copyright MIURA Masahiro 2010 - 2010.
+Distributed under the Boost Software License, Version 1.0.
+   (See accompanying file LICENSE_1_0.txt or copy at
+         http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+import std.conv;
+import std.json;
+import std.stdio;
+import std.string;
+
+string affiliation(in JSONValue[string] jsonObject)
+{
+    string kind = jsonObject["kind"].str;
+    if (kind == "module")
+	return "file:";
+
+    string name = jsonObject["name"].str;
+    switch (kind) {
+    case "class", "struct", "enum", "union":
+	return kind ~ ":" ~ name;
+    case "template":
+	return "kind:template";	// XXX nonstandard tagfield
+    case "interface":
+	return "class:" ~ name;	// XXX
+    default:
+	writefln("DEBUG: kind=[%s]", kind);
+	assert(false);
+    }
+}
+
+void convertJSONObject(in string tagFile, ref string[] tagLines,
+    in JSONValue[string] jsonObject, in JSONValue[string] parentJsonObject)
+{
+    auto m = "members" in jsonObject;
+    if (m != null) {
+	string kind = jsonObject["kind"].str;
+	if ((kind != "module") && (kind != "template")) {
+	    string newLine = format("%s\t%s\t%d;\"\t",
+		jsonObject["name"].str, tagFile, jsonObject["line"].integer);
+
+	    switch (kind) {
+	    case "class", "interface":
+		newLine ~= "c\t" ~ affiliation(parentJsonObject);
+		break;
+	    case "struct":
+		newLine ~= "s\t" ~ affiliation(parentJsonObject);
+		break;
+	    case "union":
+		newLine ~= "u\t" ~ affiliation(parentJsonObject);
+		break;
+	    case "enum":
+		newLine ~= "g\t" ~ affiliation(parentJsonObject);
+		break;
+	    default:
+		assert(false);
+	    }
+	    tagLines ~= newLine;
+	}
+
+	foreach (member; m.array)
+	    convertJSONObject(tagFile, tagLines, member.object, jsonObject);
+	return;
+    }
+
+    if (("name" !in jsonObject) || ("line" !in jsonObject))
+	return;
+    auto name = jsonObject["name"].str;
+    if (name.indexOf(' ') != -1)
+	return;
+
+    string newLine = format("%s\t%s\t%d;\"\t",
+	name, tagFile, jsonObject["line"].integer);
+    string parentName;
+    // Note: "module" object may lack "name".
+    if (parentJsonObject["kind"].str == "module")
+	parentName = parentJsonObject["file"].str;
+    else parentName = parentJsonObject["name"].str;
+
+    switch (jsonObject["kind"].str) {
+    case "alias":
+	newLine ~= "t\t" ~ affiliation(parentJsonObject);
+	break;
+    case "constructor":
+	// Special case: Use the class name as the tagname.
+	newLine = format("%s\t%s\t%d;\"\tm\tclass:%s",
+	    parentName, tagFile, jsonObject["line"].integer, parentName);
+	break;
+    case "enum member":
+	newLine ~= "e\tenum:" ~ parentName;
+	break;
+    case "function":
+	newLine ~= "f\t" ~ affiliation(parentJsonObject);
+	break;
+    case "struct":
+	newLine ~= "s\t" ~ affiliation(parentJsonObject);
+    case "typedef":
+	newLine ~= "t\t" ~ affiliation(parentJsonObject);
+    case "variable":
+	switch (parentJsonObject["kind"].str) {
+	case "class", "struct":
+	    newLine ~= "m\t" ~ affiliation(parentJsonObject);
+	    break;
+	default:
+	    newLine ~= "v\t" ~ affiliation(parentJsonObject);
+	    break;
+	}
+	break;
+    default:
+	writefln("DEBUG: kind=[%s]", jsonObject["kind"].str);
+	writefln("DEBUG: name=[%s]", jsonObject["name"].str);
+	assert(false);
+    }
+    tagLines ~= newLine;
+    return;
+}
+
+void main(string[] args)
+{
+    string[] tagLines;
+
+    // Read and parse the JSON file content.
+    auto f = File(args[1], "r");
+    string s = "";
+    foreach (ubyte[] buf; f.byChunk(4096))
+	s ~= buf;
+    f.close;
+    JSONValue val = parseJSON(s);
+
+    // for each D source file...
+    foreach (JSONValue v; val.array) {
+	JSONValue[string] srcFileObject = v.object;
+
+	auto tagFile = srcFileObject["file"].str;
+	convertJSONObject(tagFile, tagLines, srcFileObject, null);
+    }
+
+    tagLines.sort;
+    foreach (line; tagLines) {
+	writeln(line);
+    }
+}
+
+// eof
